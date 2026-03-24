33e91f80eab926edacec4089bf7e397eae2694a1
Charulatha <charugt17@gmail.com>
2026-03-24 21:53:21 +0530

    Counter

---
+++ src/components/Counter.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from "react";
+
+const Counter = () => {
+    const [responseData, setResponseData] = useState();
+
+    // const fetchData = () => {
+    //     return new Promise((resolve, reject) => {
+
+    //     })
+    // }
+
+    // }
+
+    return (
+        <></>
+        // <div>
+        //     <button onClick={() => handleStart()}>Start</button>
+        //     <button onClick={() => handleReset()}>Reset</button>
+        //     <button onClick={() => handleStop()}>Stop</button>
+        // </div>
+
+    )
+}
+
+export default Counter;
--- src/pages/DataGrid.jsx
+++ src/pages/DataGrid.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import GridWithTable from '../components/GridWithTable';
-import GridWithVirtualized from '../components/GridWithVirtualized'
+import GridWithVirtualized from '../components/GridWithVirtualized';
+import Counter from '../components/Counter';
 
 const DataGrid = () => {
     const [isGrid, setIsGrid] = useState('');
@@ -9,17 +10,36 @@ const DataGrid = () => {
         setIsGrid(type);
     }
 
+// string reversal 
+    // let a = 'charu';
+    // let b = 'charu is my name';
+    // console.log(a.split('').reverse().join(''));
+    // console.log(b.split(' '));
+    // let c = b.split(' ');
+    // let d = '';
+    // for (let i = 0; i < c.length; i++) {
+    //     console.log(c[i], 'forLoop');
+    //     console.log(c[i].split(''), 'forLoop2');
+    //     console.log(c[i].split('').reverse(), 'forLoop3');
+    //     console.log(c[i].split('').reverse().join(''), 'forLoop3');
+    //     d = d + " " + c[i].split('').reverse().join('')
+    // }
+    // console.log(d, 'endesult')
+
     return (
         <>
         <div>
             <button onClick={() => handleNormalClick('normal')}> Normal Grid </button>
             <button onClick={() => handleNormalClick('virtual')}> Virtualized Grid </button>
+            <button onClick={() => handleNormalClick('button')}>Counter</button>
         </div>
         {
             isGrid === 'normal' && 
             <GridWithTable /> }
              {isGrid === 'virtual' &&
             <GridWithVirtualized />}
+            {isGrid === 'button' &&
+            <Counter />}
         
         </>
     )
